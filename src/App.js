d85f74fca5c773ed1f388924e9118fd9343ee5fb
John Doe <johndoe71@outlook.com>
2020-08-17 16:57:23 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
+
+import  Clock from './components/Clock';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
+     
+
       <header className="App-header">
+        <h1>I <span></span>❤️ React js</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <Clock />
+      
       </header>
+      
     </div>
   );
 }
